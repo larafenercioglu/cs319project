d9c86078ef9abd851cad6a0624af3c1c740bbb54
Gökhan Taş <tasgokhan389@hotmail.com>
2021-04-13 02:57:28 +0300

    Finalized Demo added

--- Demo/app.js
+++ Demo/app.js
@@ -1,15 +1,41 @@
 localStorage.setItem("mail", "gokhan.tas@ug.bilkent.edu.tr");
-localStorage.setItem("pw", "test123");
+localStorage.setItem("pw", "testing123");
 
-function getInputValue(){
+function getInputValue() {
     // Selecting the input element and get its value 
     var email = document.getElementById("input_email").value;
     var password = document.getElementById("input_password").value;
 
-    if(email == localStorage.getItem("mail") && password == localStorage.getItem("pw")) {
-        alert("İÇERDESİN");
+    if (email == localStorage.getItem("mail") && password == localStorage.getItem("pw")) {
+        alert("Redirecting...");
     }
     else {
-        alert("sie");
+        alert("Access Denied!!!");
+    }
+}
+function sendMessage() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var msg = document.getElementById("subject").value;
+    alert("Your message has been send");
+}
+
+function openSideNav(clicked_tag) {
+
+    if( clicked_tag == "AboutUs" ) {
+        document.getElementById("mySideAboutUs").style.width = "34%";
+    }
+    else {
+        document.getElementById("mySideContact").style.width = "34%";
+    }
+}
+
+function closeSideNav(clicked_tag) {
+
+    if( clicked_tag.parentNode.id == "mySideAboutUs" ) {
+        document.getElementById("mySideAboutUs").style.width = "0";
+    }
+    else {
+        document.getElementById("mySideContact").style.width = "0";
     }
 }
