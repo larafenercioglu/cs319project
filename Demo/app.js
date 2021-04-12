1edf860d2848dc01a3df453c33d715b21b9cdf1a
Gökhan Taş <tasgokhan389@hotmail.com>
2021-04-12 22:46:09 +0300

    First demo added

---
+++ Demo/app.js
@@ -0,0 +1,15 @@
+localStorage.setItem("mail", "gokhan.tas@ug.bilkent.edu.tr");
+localStorage.setItem("pw", "kimdirbuyaprak");
+
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var email = document.getElementById("input_email").value;
+    var password = document.getElementById("input_password").value;
+
+    if(email == localStorage.getItem("mail") && password == localStorage.getItem("pw")) {
+        alert("İÇERDESİN");
+    }
+    else {
+        alert("sie");
+    }
+}
